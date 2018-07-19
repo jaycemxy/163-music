9dda887f56c0c5ba400c2386f2ad15a1bdc95c42
jaycemxy <jayce_ma.xa@foxmail.com>
2018-07-19 13:30:50 +0800

    点击歌曲激活歌曲项

--- src/js/song-list.js
+++ src/js/song-list.js
@@ -9,11 +9,15 @@
             let $el = $(this.el)
             $el.html(this.template)
             let {songs} = data
-            let liList = songs.map((song)=> $('<li></li>').text(song.name))
+            let liList = songs.map((song)=> $('<li></li>').text(song.name).attr('data-song-id',song.id))
             $el.find('ul').empty()
             liList.map((domLi)=>{
                 $el.find('ul').append(domLi)
             })
+        },
+        activeItem(li){
+            let $li = $(li)
+            $li.addClass('active').siblings('.active').removeClass('active')
         },
         clearActive(){
             $(this.el).find('.active').removeClass('active')
@@ -22,6 +26,15 @@
     let model = {
         data: {
             songs:[]
+        },
+        find(){
+            var query = new AV.Query('Song');
+            return query.find().then((songs)=>{
+                this.data.songs = songs.map((song)=>{
+                    return {id: song.id, ...song.attributes}
+                })
+                return songs
+            })
         }
     }
     let controller = {
@@ -29,6 +42,23 @@
             this.view = view
             this.model = model
             this.view.render(this.model.data)
+            this.bindEvents()
+            this.getAllSongs()
+            this.bindEventHub()
+        },
+        getAllSongs(){
+            return this.model.find().then(()=>{
+                this.view.render(this.model.data)
+            })
+        },
+        bindEvents(){
+            $(this.view.el).on('click', 'li', (e)=>{
+                this.view.activeItem(e.currentTarget)
+                let songId = e.currentTarget.getAttribute('data-song-id')
+                window.eventHub.emit('select', {id:songId})
+            })
+        },
+        bindEventHub(){
             window.eventHub.on('upload', ()=>{
                 this.view.clearActive()
             })
