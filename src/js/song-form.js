9f1b3335f01983a50608d38c9e38f38f743f97c2
jaycemxy <jayce_ma.xa@foxmail.com>
2018-07-19 14:14:42 +0800

    选中歌曲在右侧展示详情

--- src/js/song-form.js
+++ src/js/song-form.js
@@ -74,6 +74,10 @@
                 this.model.data = data
                 this.view.render(this.model.data)
             })
+            window.eventHub.on('select', (data)=> {
+                this.model.data = data
+                this.view.render(this.model.data)
+            })
         },
         bindEvents() {
             this.view.$el.on('submit', 'form', (e) => {
